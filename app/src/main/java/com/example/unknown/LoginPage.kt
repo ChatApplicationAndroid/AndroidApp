167c9507c4c97f10a8859afc7809a20d668a44e6
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2023-09-16 16:21:00 -0400

    Login Page Working

--- app/src/main/java/com/example/unknown/LoginPage.kt
+++ app/src/main/java/com/example/unknown/LoginPage.kt
@@ -8,11 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,9 +29,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+fun checkLoginCredentials(username: String, password: String): Boolean {
+    return username == "admin" && password == "admin"
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginPage(navController: NavController) {
@@ -42,6 +54,9 @@ fun LoginPage(navController: NavController) {
     {
         var username by rememberSaveable { mutableStateOf("") }
         var password by rememberSaveable { mutableStateOf("") }
+        var loginError by remember { mutableStateOf(false) }
+        var passwordHidden by rememberSaveable { mutableStateOf(true) }
+
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
@@ -63,7 +78,27 @@ fun LoginPage(navController: NavController) {
             OutlinedTextField(value = username, onValueChange = { username = it})
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "Password")
-            OutlinedTextField(value = password, onValueChange = { password = it})
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it},
+                visualTransformation =
+                if (passwordHidden) PasswordVisualTransformation() else VisualTransformation.None,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    keyboardType = KeyboardType.Password
+                ),
+                trailingIcon = {
+                    IconButton(onClick = { passwordHidden = !passwordHidden }) {
+                        val visibilityIcon =
+                            if (passwordHidden) Icons.Default.Favorite
+                            else Icons.Default.FavoriteBorder
+
+                        val description = if (passwordHidden) "Show password" else "Hide password"
+
+                        Icon(imageVector = visibilityIcon, contentDescription = description)
+                    }
+                },
+            )
         }
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -71,9 +106,48 @@ fun LoginPage(navController: NavController) {
             modifier = Modifier
             .fillMaxWidth()
             .padding(start = 32.dp, end = 32.dp),
-            onClick = { }) {
+            onClick = {
+                val isValidLogin = checkLoginCredentials(username, password)
+                if (isValidLogin) {
+                    // Navigate to the next screen on successful login
+                    navController.navigate("register_page"){
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                } else {
+                    // Display an error message or handle the error as needed
+                    loginError = true
+                    // Clear the username and password fields on incorrect login
+                    username = ""
+                    password = ""
+                }
+            }) {
             Text(
                 text = "Login"
+            )
+        }
+        if (loginError) {
+            AlertDialog(
+                onDismissRequest = {
+                    // Dismiss the dialog when the user clicks outside of it
+                    loginError = false
+                },
+                title = {
+                    Text(text = "Login Failed")
+                },
+                text = {
+                    Text(text = "Invalid username or password. Please try again.")
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            // Dismiss the dialog when the "OK" button is clicked
+                            loginError = false
+                        }
+                    ) {
+                        Text(text = "OK")
+                    }
+                }
             )
         }
 
